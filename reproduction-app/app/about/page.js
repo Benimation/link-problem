04151a6b21859911840e25b5267270487afd65ac
Ben Bout <ben@livewallgroup.com>
2023-05-28 15:53:41 +0200

    Add 'About' page using the app directory

---
+++ reproduction-app/app/about/page.js
@@ -0,0 +1,13 @@
+'use client';
+
+import Button from 'component-library/components/Button';
+
+function About() {
+  return (
+    <Button>
+      Hello Again. 
+    </Button>
+  );
+}
+
+export default About;
